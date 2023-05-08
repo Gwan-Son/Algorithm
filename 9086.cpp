a31fba17ca76e51c0235bf592789dba42df4876f
GwanSon <38202152+Gwan-Son@users.noreply.github.com>
2023-05-08 12:24:03 +0900

    Create 9086.cpp

---
+++ 9086.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        string s;
+        cin >> s;
+        cout << s.front() << s.back() << endl;
+    }
+    return 0;
+}
