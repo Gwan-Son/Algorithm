c220d146ee4c524ff7fc9093b4450c5c8c0dbc02
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2024-10-05 03:47:17 +0900

    [Silver III] Title: N과 M (1), Time: 88 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -0,0 +1,22 @@
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+
+let N = input[0]
+let M = input[1]
+
+var stack = [Int]()
+
+func dfs() {
+    if stack.count == M {
+        print(stack.map{ String($0)}.joined(separator: " "))
+        return
+    }
+    for i in 1...N {
+        if !stack.contains(i) {
+            stack.append(i)
+            dfs()
+            stack.removeLast()
+        }
+    }
+}
+
+dfs()
