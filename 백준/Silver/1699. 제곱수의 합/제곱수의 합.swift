c9ea5b041a6098834ef316d50335c16cab68ba51
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2024-10-10 02:34:18 +0900

    [Silver II] Title: 제곱수의 합, Time: 376 ms, Memory: 80292 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var dp: [Int] = Array(repeating: 100, count: 100001)
+
+dp[0] = 0
+dp[1] = 1
+for i in stride(from: 2, to: n + 1, by: 1) {
+    let sqrtNum = sqrt(Double(i))
+    if sqrtNum - Double(Int(sqrtNum)) == 0.0 {
+        dp[i] = 1
+    } else {
+        for j in stride(from: 1, through: Int(sqrtNum), by: 1) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+        }
+    }
+}
+
+print(dp[n])
