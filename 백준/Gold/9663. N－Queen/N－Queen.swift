41faebff7b35013e1e96dc2a0af3e2b152622fcc
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2024-10-31 11:34:56 +0900

    [Gold IV] Title: N-Queen, Time: 7876 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.swift
+++ 백준/Gold/9663. N－Queen/N－Queen.swift
@@ -1,42 +1,38 @@
 import Foundation
 
 let N = Int(readLine()!)!
-// swift 시간초과 개빡쳐서 나중에 다시 해결함.
-let nqueen = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184]
 
-print(nqueen[N])
-//// 각 퀸들의 위치
-//var qPos: [Int] = Array(repeating: 0, count: 16)
-//var count = 0
-//
-//func nqueen(col: Int) {
-//    if col == N {
-//        count += 1
-//        return
-//    }
-//    
-//    for i in 0..<N {
-//        var check = true
-//        for j in 0..<col {
-//            qPos[col] = i
-//            // chessboard[j] == chessboard[row] - 같은 행인지
-//            if (qPos[j] == qPos[col]) || ((j - col) == (qPos[j] - qPos[col])) || ((j - col) == (qPos[col] - qPos[j])) {
-//                check = false
-//                break
-//            }
-//        }
-//        
-//        if check {
-//            nqueen(col: col + 1)
-//        }
-//    }
-//}
-//
-//// 첫번 째 퀸의 위치
-//for i in 0..<N {
-//    qPos[0] = i
-//    nqueen(col: 1)
-//}
-//
-//print(count)
-//
+// 각 퀸들의 위치
+var qPos: [Int] = Array(repeating: -1, count: 16)
+var visited: [Bool] = Array(repeating: false, count: 16)
+var count = 0
+
+func checkPos(col: Int) -> Bool {
+    for i in 0..<col {
+        if qPos[i] == qPos[col] || abs(qPos[col] - qPos[i]) == abs(col - i) {
+            return false
+        }
+    }
+    return true
+}
+
+func nqueen(col: Int) {
+    if col == N {
+        count += 1
+        return
+    }
+    
+    for i in 0..<N {
+        if visited[i] { continue }
+        qPos[col] = i
+        if checkPos(col: col) {
+            visited[i] = true
+            nqueen(col: col + 1)
+            visited[i] = false
+        }
+    }
+}
+
+nqueen(col: 0)
+print(count)
+
