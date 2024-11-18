11816e8ba16f35afe116864d6310867d8d1d0730
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2024-11-18 16:30:04 +0900

    [Gold II] Title: 피보나치 수 3, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+var n = Int(readLine()!)!
+
+var fibonacci: [[Int64]] = [[1,1],[1,0]]
+var result = [[Int64]](repeating: [Int64](repeating: 0, count: fibonacci.count), count: fibonacci.count)
+var temp = [[Int64]](repeating: [Int64](repeating: 0, count: fibonacci.count), count: fibonacci.count)
+
+for i in 0..<2 {
+    result[i][i] = 1
+}
+
+func multiply(_ x: inout [[Int64]], _ y: [[Int64]]){
+    for i in 0..<2 {
+        for j in 0..<2 {
+            temp[i][j] = 0
+            for k in 0..<2 {
+                temp[i][j] += x[i][k] * y[k][j]
+            }
+            temp[i][j] %= 1000000
+        }
+    }
+    for i in 0..<2 {
+        for j in 0..<2 {
+            x[i][j] = temp[i][j]
+        }
+    }
+}
+
+
+while n > 0 {
+    if n % 2 == 1 {
+        multiply(&result, fibonacci)
+    }
+    multiply(&fibonacci, fibonacci)
+    n /= 2
+}
+
+print(result[0][1])
