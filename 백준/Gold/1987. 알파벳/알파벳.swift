0573e197b6ad1778d3014add9006f3d328d174b9
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2024-12-13 17:39:17 +0900

    [Gold IV] Title: 알파벳, Time: 1324 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let r = input[0]
+let c = input[1]
+
+var map = [[Int]]()
+let direction: [(y: Int, x: Int)] = [(0,-1), (0,1), (-1,0), (1,0)]
+var result = 0
+
+for _ in 0..<r {
+    map.append(readLine()!.map { Int($0.asciiValue!) - 65 })
+}
+
+func dfs(start: (y: Int, x: Int), count: Int, bit: Int) {
+    result = result > count ? result : count
+    for i in 0..<direction.count {
+        let nextP: (y: Int, x: Int) = (
+            start.y + direction[i].y,
+            start.x + direction[i].x
+        )
+    
+        if (0..<r).contains(nextP.y) && (0..<c).contains(nextP.x) {
+            let newB = 1 << map[nextP.y][nextP.x]
+            if bit & newB == 0 {
+                dfs(start: nextP, count: count + 1, bit: bit | newB)
+            }
+        }
+    }
+}
+
+dfs(start: (0,0), count: 1, bit: 1 << map[0][0])
+print(result)
