0d3108258159fd61eec147457bff3165d7f3ec49
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-01-31 14:48:33 +0900

    [level 3] Title: 여행경로, Time: 0.16 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ tickets:[[String]]) -> [String] {
+    let tickets = tickets.sorted { $0[1] < $1[1] }
+    var visited = [Bool](repeating: false, count: tickets.count)
+    var result: [String] = []
+    func dfs(_ start: String) {
+        if result.count == tickets.count {
+            result.append(start)
+            return
+        }
+        
+        for i in 0..<tickets.count {
+            if tickets[i][0] == start && !visited[i] {
+                visited[i] = true
+                result.append(start)
+                dfs(tickets[i][1])
+                if result.count == tickets.count + 1 {
+                    return
+                }
+                result.removeLast()
+                visited[i] = false
+            }
+        }
+    }
+    dfs("ICN")
+    return result
+}
