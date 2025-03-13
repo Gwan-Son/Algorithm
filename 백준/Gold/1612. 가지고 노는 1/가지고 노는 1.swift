fcdb84d6dc0dae4b481c30765c2e232f23a505fa
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-03-13 11:25:42 +0900

    [Gold V] Title: 가지고 노는 1, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Gold/1612. 가지고 노는 1/가지고 노는 1.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+func solution(_ n: Int) -> Int {
+    if (n % 2 == 0) || (n % 5 == 0) {
+        return -1
+    }
+    
+    var num = 0
+    var cnt = 1
+    while true {
+        num = (10 * num + 1) % n
+        if num % n == 0 {
+            return cnt
+        }
+        cnt += 1
+    }
+}
+
+print(solution(n))
