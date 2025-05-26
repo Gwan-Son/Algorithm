c6af256df79f7cae87ca0a15c38e42d594cbfa65
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-05-26 14:55:35 +0900

    [Gold II] Title: 후위 표기식, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.swift
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.swift
@@ -5,32 +5,31 @@ let input = readLine()!.map{ String($0) }
 var result = ""
 var stack = [String]()
 
+func precedence(_ op: String) -> Int {
+    switch op {
+    case "+", "-": return 1
+    case "*", "/": return 2
+    default: return 0
+    }
+}
+
 for i in input {
-    if i == "+" || i == "-" || i == "*" || i == "/" || i == "(" || i == ")" {
-        if stack.isEmpty {
-            stack.append(i)
-        } else {
-            if i == "(" {
-                stack.append(i)
-            } else if i == ")" {
-                while stack.last != "(" {
-                    result += stack.removeLast()
-                }
-                stack.removeLast()
-            } else if i == "*" || i == "/" {
-                while stack.last == "*" || stack.last == "/" {
-                    result += stack.removeLast()
-                }
-                stack.append(i)
-            } else {
-                while stack.last == "+" || stack.last == "-" || stack.last == "*" || stack.last == "/" {
-                    result += stack.removeLast()
-                }
-                stack.append(i)
-            }
-        }
-    } else {
+    if i >= "A" && i <= "Z" {
         result += i
+    } else if i == "(" {
+        stack.append(i)
+    } else if i == ")" {
+        while let last = stack.last, last != "(" {
+            result += stack.removeLast()
+        }
+        stack.removeLast()
+    } else {
+        while let last = stack.last,
+              last != "(",
+              precedence(last) >= precedence(i) {
+            result += stack.removeLast()
+        }
+        stack.append(i)
     }
 }
 
