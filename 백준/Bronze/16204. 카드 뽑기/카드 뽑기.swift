a340aa22087e8d032926b7294294111512299ec8
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-06-09 20:49:22 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (N, M, K) = (input[0], input[1], input[2])
+
+let countO = min(M, K)
+let countX = min(N - M, N - K)
+
+print(countO + countX)
