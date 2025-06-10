e93966fe0156a700f97f351b17b3a00f1680d1d3
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-06-10 19:11:45 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let input = readLine()!
+
+if input == "SONGDO" {
+    print("HIGHSCHOOL")
+} else if input == "CODE" {
+    print("MASTER")
+} else if input == "2023" {
+    print("0611")
+} else if input == "ALGORITHM" {
+    print("CONTEST")
+}
