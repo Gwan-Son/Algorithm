b6046442b262ae5a216eeb2f2ebb6f70f53857ab
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-07-09 12:12:22 +0900

    [Bronze V] Title: 피갤컵, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+switch n {
+case 1:
+    print("2024 8")
+    break
+case 2:
+    print("2025 3")
+    break
+case 3:
+	print("2025 10")
+    break
+case 4:
+    print("2026 5")
+    break
+case 5:
+    print("2026 12")
+    break
+default:
+    break
+}
