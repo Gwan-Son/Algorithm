cabf8cc1288a1a45d55ccb9e100d1e3ac8075007
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2025-09-30 17:07:32 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.22 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
@@ -4,18 +4,17 @@ func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
     let today = today.split(separator: ".").map{ Int($0)! }
     let year = today[0]
     let days = today[2] + (today[1] * 28)
-    
+
     var answer: [Int] = []
-    
+
     let termsDict = terms.map{[$0.split(separator: " ")[0] : Int($0.split(separator: " ")[1])!]}.flatMap { $0 }
-    
-    var number = 1
-    for privacy in privacies {
+
+    for (idx, privacy) in privacies.enumerated() {
         var privacy = privacy
         let category = String(privacy.removeLast())
         privacy.removeLast()
         var data = privacy.split(separator: ".").map { Int($0)! }
-        
+
         for term in termsDict {
             if category == term.key {
                 data[1] += term.value
@@ -29,20 +28,19 @@ func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
                         data[0] += data[1] / 12
                         data[1] = data[1] % 12
                     }
-                    
+
                 }
-                
+
                 if year > data[0] {
-                    answer.append(number)
+                    answer.append(idx + 1)
                     break
                 } else if year == data[0] && days >= (data[1] * 28 + data[2]) {
-                    answer.append(number)
+                    answer.append(idx + 1)
                     break
                 }
             }
         }
-        number += 1
     }
-    
+
     return answer
 }
