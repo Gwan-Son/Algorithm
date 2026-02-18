b8f89b59004fe1516831d6c0d48f8256be1e983d
SimGwanHyeok <38202152+Gwan-Son@users.noreply.github.com>
2026-02-18 15:29:43 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.swift
@@ -0,0 +1,10 @@
+while let input = readLine() {
+    if input == "end" { break }
+    if input == "animal" {
+        print("Panthera tigris")
+    } else if input == "flower" {
+        print("Forsythia koreana")
+    } else if input == "tree" {
+        print("Pinus densiflora")
+    }
+}
